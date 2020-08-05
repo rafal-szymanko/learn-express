4af0b0b1539da9960b771691cbe75ad032f0aef2
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-08-05 12:08:02 +0200

    Remove unneccessary tags

--- server.js
+++ server.js
@@ -4,32 +4,32 @@ const app = express();
 const path = require('path');
 const hbs = require('express-handlebars');
 
-app.engine('hbs', hbs());
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', 'hbs');
 
 
 app.get('/', (req, res) => {
-  res.render('index', {layout: false});
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', {layout: false});
+  res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact', {layout: false});
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.render('info', {layout: false});
+  res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.render('history', { layout: false });
+  res.render('history', {layout: 'dark'});
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+  res.render('hello', {name: req.params.name });
 });
 
 app.use(express.static(path.join(__dirname, '/public')));
