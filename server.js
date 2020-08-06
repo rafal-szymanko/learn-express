66961c3576cd59e43369959e474f94181b64445c
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-08-06 12:48:12 +0200

    Add script to handle form

--- server.js
+++ server.js
@@ -4,8 +4,35 @@ const app = express();
 const path = require('path');
 const hbs = require('express-handlebars');
 
-app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public')
+  },
+
+  filename: function (req, file, cb) {
+    now = Date.now().toString();
+    newFilename = `${now}${file.originalname}`;
+    cb(null, newFilename.replace(/ /g,''));
+  }
+});
+
+const upload = multer({
+  storage: storage
+});
+
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  layoutsDir: './layouts',
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({
+  extended: false
+}));
+
 
 
 app.get('/', (req, res) => {
@@ -25,17 +52,55 @@ app.get('/info', (req, res) => {
 });
 
 app.get('/history', (req, res) => {
-  res.render('history', {layout: 'dark'});
+  res.render('history', {
+    layout: 'dark'
+  });
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', {name: req.params.name });
+  res.render('hello', {
+    name: req.params.name
+  });
+});
+
+
+
+app.post('/contact/send-message', upload.single('image'), (req, res, cb) => {
+
+  const {
+    author,
+    sender,
+    title,
+    message
+  } = req.body;
+
+  const {
+    originalname,
+    filename
+  } = req.file;
+
+  console.log(req.file)
+
+  if (author && sender && title && message) {
+    res.render('contact', {
+      isSent: true,
+      imgName: originalname,
+      fileName: filename,
+    });
+  } else {
+    res.render('contact', {
+      isError: true
+    })
+  }
 });
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+
 app.use((req, res) => {
-  res.status(404).render('404', {layout: 'dark'});;
+  res.status(404).render('404', {
+    layout: 'dark'
+  });;
 })
 
 app.listen(8000, () => {
